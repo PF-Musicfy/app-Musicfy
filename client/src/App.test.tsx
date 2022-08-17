709d8d03a3336e254970b70733cc595efb1cc74e
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-16 20:17:09 -0500

    clean

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+//import { Provider } from 'react-redux';
+//import { store } from './app/store';
 import App from './App';
 
+//<Provider store={store}>
 test('renders learn react link', () => {
   const { getByText } = render(
-    <Provider store={store}>
       <App />
-    </Provider>
   );
 
   expect(getByText(/learn/i)).toBeInTheDocument();
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+//import { Provider } from 'react-redux';
+//import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
@@ -9,11 +9,10 @@ import './index.css';
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
+//<Provider store={store}>
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <App />
-    </Provider>
   </React.StrictMode>
 );
 
