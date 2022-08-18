a4f7004a91d5e59d82ff4962c652015028091b49
Hugo Avila <hugok2k@gmail.com>
2022-08-18 00:14:44 -0300

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
+import NavBarLanding from "./components/NavBarLanding";
 
 function App() {
   return (
     <div className="App">
       La app esta funcionando con el store incluido.
+      <NavBarLanding />
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,17 @@
-import {StrictMode} from 'react';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-// ✅ now importing from react-dom/client
-import {createRoot} from 'react-dom/client';
-import { Provider } from 'react-redux';
+import App from "./App";
+import store from "./store/index";
 
-import App from './App';
-import store from './store/index';
-// 👇️ IMPORTANT: make sure to specify correct ID
-// must be the ID of the div element in your index.html file
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <Provider store={store}>
-  <StrictMode>
-    <App />
-  </StrictMode>
+    <StrictMode>
+      <App />
+    </StrictMode>
   </Provider>
 );
