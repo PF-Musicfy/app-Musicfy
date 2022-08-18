e459cc24618ac6e73a41d2ad6bd2ed673e1f8b74
Kosovomba <santiagojavierlevy@gmail.com>
2022-08-18 00:06:19 -0300

    componente registerform

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import './App.css';
+import RegisterForm from './components/RegisterForm';
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       La app esta funcionando con el store incluido.
+      <RegisterForm/>
     </div>
   );
 }
