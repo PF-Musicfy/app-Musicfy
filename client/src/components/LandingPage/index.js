f8653d0acf81295796421be9ab1026010d5af1e2
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-17 23:06:05 -0500

    guardar

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,10 +1,13 @@
 import Footer from "./Footer";
 import BodyLanding from "./Body";
+import NavBarLanding from "../NavBarLanding";
+import './landingpage.css';
 //import Player from '../Reproductor';
 //<Player />
 export default function LandingPage() {
   return (
-    <div>
+    <div className='landing'>
+      <NavBarLanding />
       <BodyLanding />
       <Footer />
     </div>
