0051f51d83c97cbfd7f1d641b3719822bb42501f
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-17 22:52:26 -0500

    pausa

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,10 +1,24 @@
 import Footer from './Footer';
-import Player from '../Reproductor';
+//import Player from '../Reproductor';
+      //<Player />
+import Login from '../Login';
+import Logout from '../Logout';
+import Profile from '../Profile';
+import { useAuth0 } from '@auth0/auth0-react'
+
 export default function LandingPage(){                            
+  const { isAuthenticated } = useAuth0();
+
   return (                                                        
     <div>
+      {isAuthenticated ?
+        <>
+          <Profile />
+          <Logout />
+        </>
+        : <Login />
+      }
       <Footer />
-      <Player />
     </div>
   )                                                               
 }  
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import './index.css';
 import {StrictMode} from 'react';
-
 import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
+import { Auth0Provider } from '@auth0/auth0-react'
 
 import App from './App';
 import store from './store/index';
@@ -15,7 +15,13 @@ root.render(
   <Provider store={store}>
     <StrictMode>
       <BrowserRouter>
+        <Auth0Provider
+          domain="dev-jmd0tdig.us.auth0.com"
+          clientId="1a12D4YPRwM3zKeP5sX00qkPyCogtbhB"
+          redirectUri={window.location.origin}
+        >
         <App />
+        </Auth0Provider>
       </BrowserRouter>
     </StrictMode>
   </Provider>
