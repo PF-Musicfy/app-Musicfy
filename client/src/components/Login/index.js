8ccb6bcee071d3e85e106e963a8da3a4a732b95f
Gabriel <sgabrielok01@gmail.com>
2022-08-21 20:42:40 -0300

    agrege estilos

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,110 +1,94 @@
-import './login.css';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useState } from 'react';
-import validate from '../../utils/validate.js';
-import { useNavigate } from 'react-router-dom';
+import "./login.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useState } from "react";
+import validate from "../../utils/validate.js";
+import { useNavigate } from "react-router-dom";
 
-import Logout from '../Logout';
-import Profile from '../Profile';
+import Logout from "../Logout";
+import Profile from "../Profile";
 
-export default function Login(){
+export default function Login() {
   const { loginWithPopup, isAuthenticated } = useAuth0();
   const navigate = useNavigate();
 
   const [input, setInput] = useState({
-    user: '',
-    pass: '',
+    usernme: "",
+    password: "",
   });
   const [errors, setErrors] = useState({});
 
   const inputChange = (e) => {
-    const {name, value} = e.target;
-    setInput({...input, [name]: value})
-    setErrors(validate({...input, [name]: value}))
-  }
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+    setErrors(validate({ ...input, [name]: value }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert('mandar form al backend');
-  }
+    alert("mandar form al backend");
+    console.log(input);
+  };
 
   return (
     <div className="login">
       <div className="login-logo">
-        <img
-          src=''
-          alt='Musicfy Logo'
-          onClick={() => navigate('/')}
-        />
+        <img src="" alt="Musicfy Logo" onClick={() => navigate("/")} />
       </div>
       <hr></hr>
       <div className="login-container">
-        <button
-          onClick={() => navigate(-1)}
-        >
-          volver atras
-        </button>
+        <button onClick={() => navigate(-1)}>volver atras</button>
         <div className="login-options">
-
-          {isAuthenticated ?
+          {isAuthenticated ? (
             <>
               <Profile />
               <Logout />
             </>
-            : 
+          ) : (
             <>
               <p>Para continuar, inicia sesión en Musicfy.</p>
               <p>probar en Auth0 con:</p>
-              <span><b>email:</b> jaeden31z_d781g@cguf.site</span>
-              <p><b>password:</b> ASdf1234</p>
-              <button
-                onClick={loginWithPopup}
-              >
-                CONTINUAR CON AUTH0
-              </button>
+              <span>
+                <b>email:</b> jaeden31z_d781g@cguf.site
+              </span>
+              <p>
+                <b>password:</b> ASdf1234
+              </p>
+              <button onClick={loginWithPopup}>CONTINUAR CON AUTH0</button>
             </>
-          }
-
+          )}
         </div>
         <hr></hr>
-        <form
-          className="login-form"
-          onSubmit={handleSubmit}
-        >
+        <form className="login-form" onSubmit={handleSubmit}>
           <label>Correo electrónico o nombre de usuario</label>
           <input
-            type='text'
-            name="user"
+            type="text"
+            name="username"
             placeholder="Correo electronico o nombre de usuario"
             onChange={inputChange}
             value={input.user}
-            className={errors.user ? 'input-err' : ''}
+            className={errors.user ? "input-err" : ""}
           />
-          <p className='msg-err'>{errors.user || ''}</p>
+          <p className="msg-err">{errors.user || ""}</p>
           <label>Contraseña</label>
           <input
-            type='password'
-            name="pass"
+            type="password"
+            name="password"
             placeholder="Contraseña"
             onChange={inputChange}
             value={input.pass}
-            className={errors.pass ? 'input-err' : ''}
+            className={errors.pass ? "input-err" : ""}
           />
-          <p className='msg-err'>{errors.pass || ''}</p>
+          <p className="msg-err">{errors.pass || ""}</p>
           <div>
-            <button>
-              INICIAR SESION
-            </button>
+            <button>INICIAR SESION</button>
           </div>
         </form>
         <hr></hr>
         <p>¿No tienes cuenta?</p>
-        <button
-          onClick={() => navigate('/register')}
-        >
+        <button onClick={() => navigate("/register")}>
           REGISTRATE EN MUSICFY
         </button>
       </div>
     </div>
-  )
+  );
 }
