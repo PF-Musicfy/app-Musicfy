cf364c470cca7834a5e8b7540f52fccf6cb8dd06
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-23 19:17:35 -0500

    refactor(client/src/components/Premium)!: Dont repeat yourself

---
+++ client/src/components/CardPlan/index.jsx
@@ -0,0 +1,35 @@
+import styles from "../Premium/premium.module.css";
+import { MdComputer } from "react-icons/md";
+import { RiComputerLine } from "react-icons/ri";
+import { BsTabletLandscape } from "react-icons/bs";
+import { HiOutlineDeviceTablet } from "react-icons/hi";
+import { FiCheck } from "react-icons/fi";
+
+export default function CardPlan({ plan }) {
+  return (
+    <div className={styles.card}>
+      <h1>{plan}</h1>
+      <div className={styles.icons}>
+        <HiOutlineDeviceTablet />
+        <BsTabletLandscape className={styles.iconito} />
+        <MdComputer className={styles.iconito} />
+        <RiComputerLine className={styles.iconito} />
+      </div>
+      <div className={styles.cardwords}>
+        <p>
+          <FiCheck />
+          Choose your music
+        </p>
+        <p>
+          <FiCheck />
+          Create your own playlists
+        </p>
+        <p>
+          <FiCheck />
+          Music without publicity
+        </p>
+      </div>
+      <button>Choose plan</button>
+    </div>
+  )
+}
