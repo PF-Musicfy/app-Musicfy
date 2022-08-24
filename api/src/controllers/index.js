83d9144a1604dd261d8586c4b6c0e0ebb53e5f29
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-24 17:54:01 -0500

    refactor(api/src/controllers/index.js)!: destructuring

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -1,85 +1,69 @@
 const axios = require("axios");
-  const apiKey = 'YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4';
+  const apiKey = 'apikey=YTkxZTRhNzAtODdlNy00ZjMzLTg0MWItOTc0NmZmNjU4Yzk4';
   const urlNapster = 'https://api.napster.com/imageserver/v2';
   const mainUrl = 'https://api.napster.com/v2.2'
 
   const topMusic = async () => {
+    const {data: {tracks}} = await axios.get(`${mainUrl}/tracks/top?${apiKey}&limit=10`);
+    const apiTracks = await tracks.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        artistName: e.artistName,
+        previewURL: e.previewURL,
+        albumName: e.albumName,
+        images: `${urlNapster}/artists/${e.artistId}/images/356x237.jpg`
+      };
+    });
 
-  const tracks = await axios.get(
-    `${mainUrl}/tracks/top?apikey=${apiKey}&limit=10`
-  );
-  const apiTracks = await tracks.data.tracks.map((e) => {
-    return {
-      id: e.id,
-      name: e.name,
-      artistName: e.artistName,
-      previewURL: e.previewURL,
-      albumName: e.albumName,
-      images: `${urlNapster}/artists/${e.artistId}/images/356x237.jpg`
-    };
+    const {data: {albums}} = await axios.get(`${mainUrl}/albums/new?${apiKey}&limit=10`);
+    const apiAlbums = await albums.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        artistName: e.artistName,
+        trackCount: e.trackCount,
+        images: `${urlNapster}/albums/${e.id}/images/356x237.jpg`,
+      };
+    });
 
-  });
+    const {data: {artists}} = await axios.get(`${mainUrl}/artists/top?${apiKey}&limit=10`);
+    const apiArtists = await artists.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        shortcut: e.shortcut,
+        images: `${urlNapster}/artists/${e.id}/images/356x237.jpg`,
+      };
+    });
 
-  const albums = await axios.get(
-    `${mainUrl}/albums/new?apikey=${apiKey}&limit=10`
-  );
-  const apiAlbums = await albums.data.albums.map((e) => {
-    return {
-      id: e.id,
-      name: e.name,
-      artistName: e.artistName,
-      trackCount: e.trackCount,
-      images: `${urlNapster}/albums/${e.id}/images/356x237.jpg`,
-    };
-  });
+    const {data: {playlists}} = await axios.get(`${mainUrl}/playlists/top?${apiKey}&limit=10`);
+    const apiPlaylists = await playlists.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        trackCount: e.trackCount,
+        images: `${urlNapster}/playlists/${e.id}/artists/images/356x237.jpg`,
+      };
+    });
 
-  const artists = await axios.get(
-    `${mainUrl}/artists/top?apikey=${apiKey}&limit=10`
-  );
-  const apiArtists = await artists.data.artists.map((e) => {
-    return {
-      id: e.id,
-      name: e.name,
-      shortcut: e.shortcut,
-      images: `${urlNapster}/artists/${e.id}/images/356x237.jpg`,
-    };
-  });
+    const {data: {stations}} = await axios.get(`${mainUrl}/stations/top?${apiKey}&limit=10`);
+    const apiStations = await stations.map((e) => {
+      return {
+        id: e.id,
+        name: e.name,
+        artists: e.artists,
+        summary: e.summary,
+        images: `${urlNapster}/stations/${e.id}/images/356x237.jpg`,
+      };
+    });
 
-  const playlists = await axios.get(
-    `${mainUrl}/playlists/top?apikey=${apiKey}&limit=10`
-  );
-  const apiPlaylists = await playlists.data.playlists.map((e) => {
-    return {
-      id: e.id,
-      name: e.name,
-      trackCount: e.trackCount,
-      images: `${urlNapster}/playlists/${e.id}/artists/images/356x237.jpg`,
-    };
-  });
-
-  const stations = await axios.get(
-    `${mainUrl}/stations/top?apikey=${apiKey}&limit=10`
-  );
-  const apiStations = await stations.data.stations.map((e) => {
-    return {
-      id: e.id,
-      name: e.name,
-      artists: e.artists,
-      summary: e.summary,
-      images: `${urlNapster}/stations/${e.id}/images/356x237.jpg`,
-    };
-  });
-
-  const obj = { apiTracks, apiAlbums, apiArtists, apiPlaylists, apiStations }
-
-  return obj
-
-  }
-
+    return { apiTracks, apiAlbums, apiArtists, apiPlaylists, apiStations }
+}
 
 const getByName = async (name) => {
   const byName = await axios.get(
-    `${mainUrl}/search?apikey=${apiKey}&per_type_limit=5&query=${name}`
+    `${mainUrl}/search?${apiKey}&per_type_limit=5&query=${name}`
   );
 
   const albums = byName.data.search.data.albums.map((e) => {
@@ -128,7 +112,7 @@ const getByName = async (name) => {
 
 const getTrackId = async (id) => {
   const trackId = await axios.get(
-    `${mainUrl}/tracks/${id}?apikey=${apiKey}`
+    `${mainUrl}/tracks/${id}?${apiKey}`
   );
   const track = trackId.data.tracks?.map((e) => {
       return {
@@ -144,7 +128,7 @@ const getTrackId = async (id) => {
     };
   });
   const trackAlbumId = trackId.data.tracks?.map(e => e.albumId)
-  const trackAlbum = await axios.get(`${mainUrl}/albums/${trackAlbumId}/tracks?apikey=${apiKey}`)
+  const trackAlbum = await axios.get(`${mainUrl}/albums/${trackAlbumId}/tracks?${apiKey}`)
   const tracksMusic = trackAlbum.data.tracks.map(e => {
     return{
       id: e.id,
@@ -161,7 +145,7 @@ return obj
 
 const getAlbumId = async (id)=>{
   const albumId = await axios.get(
-    `${mainUrl}/albums/${id}?apikey=${apiKey}`
+    `${mainUrl}/albums/${id}?${apiKey}`
   );
   const album = albumId.data.albums?.map((e) => {
     return {
@@ -173,7 +157,7 @@ const getAlbumId = async (id)=>{
     };
   });
   const idAlbum = albumId.data.albums?.map(e => e.id)
-  const albumTracks = await axios.get(`${mainUrl}/albums/${idAlbum}/tracks?apikey=${apiKey}`)
+  const albumTracks = await axios.get(`${mainUrl}/albums/${idAlbum}/tracks?${apiKey}`)
   const albumMusic = albumTracks.data.tracks?.map(e => {
     return{
       id: e.id,
@@ -191,7 +175,7 @@ const getAlbumId = async (id)=>{
 
 const getArtistId = async (id)=>{
   const artistId = await axios.get(
-    `${mainUrl}/artists/${id}?apikey=${apiKey}`
+    `${mainUrl}/artists/${id}?${apiKey}`
   );
 
   const artist = artistId.data.artists.map((e) => {
@@ -204,7 +188,7 @@ const getArtistId = async (id)=>{
 
   const artistAlbumId = artistId.data.artists.map(e => e.albumGroups)
   const group = artistAlbumId.map(e => e.compilations[0])
-  const artistAlbum = await axios.get(`${mainUrl}/albums/${group}/tracks?apikey=${apiKey}`)
+  const artistAlbum = await axios.get(`${mainUrl}/albums/${group}/tracks?${apiKey}`)
   const artistMusic = artistAlbum.data.tracks.map(e => {
     return{
       id: e.id,
@@ -223,7 +207,7 @@ const getArtistId = async (id)=>{
 
 const getPlaylistId = async (id)=>{
   const playlistId = await axios.get(
-    `${mainUrl}/playlists/${id}?apikey=${apiKey}`
+    `${mainUrl}/playlists/${id}?${apiKey}`
   );
   const playlist = playlistId.data.playlists.map((e) => {
     return {
@@ -235,7 +219,7 @@ const getPlaylistId = async (id)=>{
     };
   });
   const PlaylistId = playlistId.data.playlists.map(e => e.id)
-  const albumPlaylist = await axios.get(`${mainUrl}/playlists/${PlaylistId}/tracks?apikey=${apiKey}&limit=15`)
+  const albumPlaylist = await axios.get(`${mainUrl}/playlists/${PlaylistId}/tracks?${apiKey}&limit=15`)
   const playlistMusic = albumPlaylist.data.tracks.map(e => {
     return{
       id: e.id,
