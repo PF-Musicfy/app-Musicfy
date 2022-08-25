f055abccff41f19c5e58e3d55fd95c46f4bea27b
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-24 20:27:50 -0500

    feat: metodo getUsers - reducer

--- client/src/store/slice/user.js
+++ client/src/store/slice/user.js
@@ -4,26 +4,36 @@ import axios from 'axios';
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    feedback: []
+    feedback: [],
+    users: [],
   },
   reducers: {
     setFeedback: (state, action) => {
       state.feedback = action.payload.reverse();
     },
+    setUsers: (state, action) => {
+      state.users = action.payload;
+    },
   },
 })
 
-export const {setFeedback} = userSlice.actions;
+export const {
+  setFeedback,
+  setUsers,
+} = userSlice.actions;
 
 export default userSlice.reducer;
 
-export function getFeedback() {
-  return async function (dispatch) {
-    try {
-      const posts = await axios.get(`${axios.defaults.baseURL}/feedback`);
-      return dispatch(setFeedback(posts.data));
-    } catch (error) {
-      console.log('error getFeedback');
+function elCreador(url = '',cb1,cb2){
+  return async function(dispatch){
+    if(typeof cb2 === 'function') dispatch(cb2())
+    try{
+      const { data } = await axios.get(axios.defaults.baseURL + url);
+      dispatch(cb1(data));
+    }catch(e){
+      console.log('error: action ',cb1.name);
     }
-  };
+  }
 }
+export const getFeedback = () => elCreador('/feedback', setFeedback)
+export const getUsers = () => elCreador('/user', setUsers)
