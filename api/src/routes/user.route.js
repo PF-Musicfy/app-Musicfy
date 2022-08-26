eabae92d633720e2a0d77867970869947b99e5e1
Elam Cano <elamcano16@gmail.com>
2022-08-26 03:37:57 -0300

    changes

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -1,50 +1,62 @@
-const {Router} = require('express');
-const axios = require('axios');
+const { Router } = require("express");
+const axios = require("axios");
 const User = require("../models/User.js");
 
 const router = Router();
 
-router.get('/',async (req,res)=>{
-  const users = await User.find();
-  res.send(users);
-})
-router.get('/free',async (req,res)=>{
-  try{
-    console.log('free');
+router.get("/", async (req, res) => {
+  const { username } = req.query;
+  console.log(req.query);
+  if (username) {
+    let userFound = await User.find({
+      username: { $regex: username, $options: "i" },
+    }).limit(5);
+    /* if (!userFound[0]) {
+      res.status(404).send(`No user found with the search '${username}'`);
+    } */
+    return res.send(userFound);
+  } else {
+    const users = await User.find();
+    res.send(users);
+  }
+});
+router.get("/free", async (req, res) => {
+  try {
+    console.log("free");
     //const users = await User.find({$or:[ {'premium':"false"}, {'premium':false} ]});
     const users = await User.find({ premium: false });
-    res.send(users);
-  }catch (e){
-    res.status(500).send('error');
+    res.json(users);
+  } catch (e) {
+    res.status(500).send("error");
   }
-})
-router.get('/premium',async (req,res)=>{
+});
+router.get("/premium", async (req, res) => {
   const users = await User.find({ premium: true });
   res.send(users);
-})
-router.get('/admin',async (req,res)=>{
+});
+router.get("/admin", async (req, res) => {
   const users = await User.find({ admin: true });
   res.send(users);
-})
-router.post('/online',async (req,res) => {
-  try{
+});
+router.post("/online", async (req, res) => {
+  try {
     const { id } = req.body;
     let user = await User.findById(id);
     user.online = !user.online;
-    await user.save()
+    await user.save();
     console.log(`user: ${id} | ${user.online}`);
     res.send(user.online);
-  }catch(e){
-    res.status(500).send('error post/online');
+  } catch (e) {
+    res.status(500).send("error post/online");
   }
-})
-router.get('/online/:id',async (req,res) => {
-  try{
+});
+router.get("/online/:id", async (req, res) => {
+  try {
     const { id } = req.params;
     let user = await User.findById(id);
     res.send(user.online);
-  }catch(e){
-    res.status(500).send('error get/online');
+  } catch (e) {
+    res.status(500).send("error get/online");
   }
-})
+});
 module.exports = router;
