26d38e6192cc5bd38f699953893a684ec208149e
tm10ymhp <tm10ymhp@protonmail.com>
2022-08-28 11:46:01 -0500

    feat: dashboard solo para usuarios logeados

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,10 +1,76 @@
 import { Link, Route, Routes } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useState, useRef, useEffect } from "react";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import st from "../Login/login.module.css";
 
 import s from "./dashboard.module.css";
+import { userTokenInfo } from "../../store/slice/user";
 import SideBar from "../../components/SideBar";
 import DashboardHome from "./DashboardHome.jsx";
 import DashboardFeedback from "./DashboardFeedback.jsx";
+
+function LoginForm() {
+  const inputPass = useRef();
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios.post(`${axios.defaults.baseURL}/api/v1/auth/login`, {
+      email: e.target[0].value,
+      password: e.target[1].value,
+    },{
+      withCredentials: true
+    })
+    .then(() => {
+      alert("logeado");
+      dispatch(userTokenInfo())
+    })
+    .catch((e) => {
+      alert("posibles errores:\n" +
+          "- el back no se ha iniciado\n" +
+          "- alguno de los campos falta o es incorrecto\n" +
+          "- el usuario no existe en la base de datos"
+      );
+    });
+  };
+  return (
+    <div className={st.container}>
+      <form className={st.form} onSubmit={handleSubmit}>
+        <p>Email</p>
+        <input
+          type="text"
+          name="user"
+          placeholder="Email"
+        />
+        <p>Password</p>
+        <input
+          ref={inputPass}
+          type="password"
+          name="pass"
+          placeholder="Password"
+        />
+        <button
+          type="button"
+          className={st.button}
+          onClick={() => {
+            if (inputPass.current.type === "password") {
+              inputPass.current.type = "text";
+            } else {
+              inputPass.current.type = "password";
+            }
+          }}
+        >
+          mostrar contraseña
+        </button>
+        <div className={st.containerSend}>
+          <button className={st.btnSend}>Login</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 export default function Dashboard() {
   const { user } = useSelector(state => state.user)
@@ -23,7 +89,7 @@ export default function Dashboard() {
         <Route exact path="/feedback" element={<DashboardFeedback />} />
       </Routes>
       </>
-      : 'debe iniciar sesion'
+      : <LoginForm />
       }
     </div>
   )
