23ed0f9bee52094875a43ec0055d2667b4a6198b
Kosovomba <santiagojavierlevy@gmail.com>
2022-08-31 06:15:00 -0300

    el componente validate renderiza un mensaje adecuado según si se registró exitosamente o no

--- client/src/components/Validation/index.jsx
+++ client/src/components/Validation/index.jsx
@@ -2,6 +2,8 @@ import { useParams, useLocation } from "react-router-dom";
 import axios from "axios";
 import Swal from 'sweetalert2';
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Validation() {
     const {email, username} = useParams()
@@ -22,9 +24,12 @@ export default function Validation() {
     console.log(location)
     let password = location.pathname.split('/').splice(4).join('/')
     console.log("password:", password)
-
+    const [state, setState] = useState({})
+    useEffect(()=> {
+    
     axios.post(`${axios.defaults.baseURL}/api/v1/auth/register`, {email, username, password})
     .then(() => {
+        setState({success: 1})
         Toast.fire({
             icon: 'success',
             title: 'User registered succesfully'
@@ -36,17 +41,21 @@ export default function Validation() {
             navigate('/')
         }, 2000);
     })
-    .catch((error) => {
-        console.log(error)
+    .catch((err) => {
+        setState({error: 1})
+        console.log(err)
         Swal.fire({
             icon: 'error',
             title: 'Oops try again!',
             text: 'Email already registered',
           })
     })
+
+    }, [])
     return (
         <div>
-            Register succesful!
+            {state.error? <p>Register failed</p> : false}
+            {state.success? <p>Register succesful! </p> : false}
         </div>
     )
 }
