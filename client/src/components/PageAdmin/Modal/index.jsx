662d398f175d8cbe7fd8f2c66d0010843a7e68b1
Kosovomba <santiagojavierlevy@gmail.com>
2022-09-03 21:32:35 -0300

    fix user en modal de send-message

--- client/src/components/PageAdmin/Modal/index.jsx
+++ client/src/components/PageAdmin/Modal/index.jsx
@@ -4,10 +4,11 @@ import axios from "axios";
 import { useSelector } from "react-redux";
 import { useState } from "react";
 
-export default function Modal({ closeModal }) {
+export default function Modal({ closeModal }) {  
   const { usermodal } = useSelector((state) => state.user);
   const [subject, setSubject] = useState("");
   const [text, setText] = useState("");
+  let user = usermodal[0].username
 
   const handleSubject = (e) => {
     setSubject(e.target.value);
@@ -38,7 +39,7 @@ export default function Modal({ closeModal }) {
           <div className={s.title}>
             <h1>Send a message</h1>
           </div>
-          <div className={s.user}>User: {usermodal.username}</div>
+          <div className={s.user}> User: {user} </div>
           <input
             className={s.subject}
             maxLength="20"
