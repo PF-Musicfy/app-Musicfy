257fef3bb594fcce887952a92a32d2c23358fc16
Hugo Avila <hugok2k@gmail.com>
2022-09-14 21:23:22 -0300

    update/css login and fix register

--- client/src/pages/Login/ModuleLogin.jsx
+++ client/src/pages/Login/ModuleLogin.jsx
@@ -9,7 +9,7 @@ import login from "../../utils/login.js";
 export default function ModuleLogin({ success }) {
   const [input, setInput] = useState({
     user: "",
-    pass: "",
+    pass: ""
   });
   const [errors, setErrors] = useState({});
   const [hasSubmit, setHasSubmit] = useState(false);
@@ -24,13 +24,12 @@ export default function ModuleLogin({ success }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     setHasSubmit(true);
     try {
       await login(
         {
           email: input.user,
-          password: input.pass,
+          password: input.pass
         },
         "/api/v1/auth/login",
         success
@@ -43,50 +42,37 @@ export default function ModuleLogin({ success }) {
 
   return (
     <div className={theme === "light" ? sLight.container : s.container}>
+      <span className={theme === "light" ? sLight.title : s.title}>Log in</span>
       <div className={theme === "light" ? sLight.options : s.options}>
         <LoginWithGoogle />
       </div>
-      <form
-        className={theme === "light" ? sLight.form : s.form}
-        onSubmit={handleSubmit}
-      >
-        <div className={theme === "light" ? sLight.inputs : s.inputs}>
-          <p className={theme === "light" ? sLight.emailTitle : s.emailTitle}>
-            Email
-          </p>
-          <input
-            type="email"
-            name="user"
-            placeholder="Email"
-            onChange={inputChange}
-            value={input.user}
-            className={errors.user ? s.inputError : ""}
-          />
-          <p className={theme === "light" ? sLight.msgError : s.msgError}>
-            {errors.user || ""}
-          </p>
-        </div>
+      <form className={theme === "light" ? sLight.form : s.formCon} onSubmit={handleSubmit}>
+        <label className={theme === "light" ? sLight.emailTitle : s.emailTitle}>Email</label>
+        <input
+          type="email"
+          name="user"
+          placeholder="Email"
+          onChange={inputChange}
+          value={input.user}
+          className={s.inputForm}
+        />
 
-        <div className={theme === "light" ? sLight.inputs : s.inputs}>
-          <p className={theme === "light" ? sLight.emailTitle : s.emailTitle}>
-            Password
-          </p>
-          <input
-            ref={inputPass}
-            type="password"
-            name="pass"
-            placeholder="Password"
-            onChange={inputChange}
-            value={input.pass}
-            className={errors.pass ? s.inputError : ""}
-          />
-          <p className={theme === "light" ? sLight.msgError : s.msgError}>
-            {errors.pass || ""}
-          </p>
-        </div>
-        <button
-          type="button"
-          className={theme === "light" ? sLight.button : s.button}
+        <span className={theme === "light" ? sLight.msgError : s.msgErrorEmail}>{errors.user || ""}</span>
+
+        <label className={theme === "light" ? sLight.emailTitle : s.emailTitle}>Password</label>
+        <input
+          ref={inputPass}
+          type="password"
+          name="pass"
+          placeholder="Password"
+          onChange={inputChange}
+          value={input.pass}
+          className={s.inputForm}
+        />
+        <p className={theme === "light" ? sLight.msgError : s.msgError}>{errors.pass || ""}</p>
+
+        <FaEye
+          className={theme === "light" ? sLight : s.eye}
           onClick={() => {
             if (inputPass.current.type === "password") {
               inputPass.current.type = "text";
@@ -94,17 +80,10 @@ export default function ModuleLogin({ success }) {
               inputPass.current.type = "password";
             }
           }}
-        >
-          <FaEye className={theme === "light" ? sLight : s.eye} />
-        </button>
+        />
 
-        <div
-          className={theme === "light" ? sLight.containerSend : s.containerSend}
-        >
-          <button
-            disabled={hasSubmit}
-            className={theme === "light" ? sLight.btnSend : s.btnSend}
-          >
+        <div className={s.prueba}>
+          <button disabled={hasSubmit} className={theme === "light" ? sLight.btnSend : s.buttonReg}>
             Login
           </button>
         </div>
--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { FaBackward } from "react-icons/fa";
-
 import s from "./login.module.css";
 import sLight from "./loginLight.module.css";
 import setTitle from "../../utils/setTitle.js";
@@ -10,54 +8,43 @@ import ModuleLogin from "./ModuleLogin.jsx";
 import Loading from "../../components/Loading";
 import { useLoading } from "../../hooks/useLoading.js";
 import Footer from "components/LandingPage/Footer";
+import NavBarLandingOn from "../../components/LandingPage/NavBarLandingOn";
+import NavBarLandingOff from "../../components/LandingPage/NavBarLandingOff";
+// import { Link } from "react-router-dom";
 
 export default function Login() {
   setTitle("Login - Musicfy");
-
   const { display, loading } = useLoading();
   const { user } = useSelector((state) => state.user);
   const navigate = useNavigate();
   const theme = localStorage.getItem("theme");
-
   useEffect(() => {
     if (Object.keys(user).length) {
       navigate("/home");
     }
   }, [user, navigate]);
-
   return (
     <>
-      <div>
-        {Object.keys(user).length ? (
-          ""
-        ) : loading ? (
-          <Loading text={loading} />
-        ) : (
-          <div
-            className={theme === "light" ? sLight.login : s.login}
-            style={{ display }}
-          >
-            <div className={theme === "light" ? sLight.navbar : s.navbar}>
-              <div onClick={() => navigate("/")}>
-                <img src="https://i.imgur.com/GiyjGcI.png" alt="Musicfy Logo" />
-                <span>Musicfy</span>
-              </div>
-              <button onClick={() => navigate(-1)}>
-                <FaBackward />
-              </button>
-            </div>
+      {Object.keys(user).length ? (
+        ""
+      ) : loading ? (
+        <Loading text={loading} />
+      ) : (
+        <>
+          {Object.keys(user).length ? <NavBarLandingOn /> : <NavBarLandingOff />}
+          <div className={theme === "light" ? sLight.login : s.login} style={{ display }}>
             <ModuleLogin />
-            <p>¿You do not have an account?</p>
+            <span className={s.haveAccount}>Not registered on Musicfy yet?</span>
             <button
               className={theme === "light" ? sLight.btnRegister : s.btnRegister}
               onClick={() => navigate("/register")}
             >
-              ¡ REGISTER IN MUSICFY !
+              SIGN UP
             </button>
-            <Footer />
           </div>
-        )}
-      </div>
+          <Footer />
+        </>
+      )}
     </>
   );
 }
