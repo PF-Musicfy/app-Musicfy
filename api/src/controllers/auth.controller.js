afc40b20b9c979da54f484af33afb3f242997127
Hugo Avila <hugok2k@gmail.com>
2022-09-23 16:13:05 -0300

    fix about

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -1,173 +1,163 @@
-const { findById } = require("../models/Post.js");
-const User = require("../models/User.js");
-const {
-  generateRefreshToken,
-  generateToken,
-} = require("../utils/tokenManager.js");
-const express = require("express");
+const { findById } = require('../models/Post.js')
+const User = require('../models/User.js')
+const { generateRefreshToken, generateToken } = require('../utils/tokenManager.js')
+const express = require('express')
 
 // Kosovomba
-const bcrypt = require("bcryptjs");
-const { mailTransport } = require("../controllers/mailController");
+const bcrypt = require('bcryptjs')
+const { mailTransport } = require('../controllers/mailController')
 const validate = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    let user = await User.findOne({ email });
+    const { username, email, password } = req.body
+    let user = await User.findOne({ email })
     if (user) {
-      return res.status(404).send(`${email} already exists`);
+      return res.status(404).send(`${email} already exists`)
     }
-    const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(password, salt);
-    let validationLink = `${express.defaultURL}/validate/${email}/${username}/${hashPassword}`;
-    console.log(validationLink);
-    let transporter = mailTransport();
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password, salt)
+    let validationLink = `${express.defaultURL}/validate/${email}/${username}/${hashPassword}`
+    let transporter = mailTransport()
     let mailOptions = {
-      from: "adminAPI",
+      from: 'adminAPI',
       to: email,
-      subject: "Validation link",
+      subject: 'Validation link',
       html: `<b> Hello! Click this link in order to complete registration: </b>
-    <a href= "${validationLink}">Link</a>`,
-    };
+    <a href= "${validationLink}">Link</a>`
+    }
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
-        res.status(500).send(error.message);
+        res.status(500).send(error.message)
       } else {
-        console.log("email enviado");
-        return res.status(200).send();
+        return res.status(200).send()
       }
-    });
+    })
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: error.message })
   }
-};
+}
 
 // Kosovomba
 
 const registerUser = async (req, res) => {
   try {
-    const { username, email, password } = req.body;
-    console.log(req.body);
-    let user = await User.findOne({ email });
+    const { username, email, password } = req.body
+    let user = await User.findOne({ email })
 
     // if (user) alert("Email already exists");
     if (user) {
-      return res.status(404).send(`${email} already exists`);
+      return res.status(404).send(`${email} already exists`)
     }
-    console.log(user);
-
-    user = new User({ username, email, password });
-    await user.save();
+    user = new User({ username, email, password })
+    await user.save()
 
     // genrerar jwt
-    const { token, expiresIn } = generateToken(user.id);
-    generateRefreshToken(user.id, res);
+    const { token, expiresIn } = generateToken(user.id)
+    generateRefreshToken(user.id, res)
 
-    return res.status(201).json({ token, expiresIn });
+    return res.status(201).json({ token, expiresIn })
   } catch (error) {
-    console.log(error);
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ error: error.message })
   }
-};
+}
 
 const loginUser = async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
-    let user = await User.findOne({ email });
-    if (!user) throw new Error("This user does not exist");
-    if (user.isblocked) return res.status(500).send("This user is blocked");
+    let user = await User.findOne({ email })
+    if (!user) throw new Error('This user does not exist')
+    if (user.isblocked) return res.status(500).send('This user is blocked')
 
-    const responsePassword = await user.comparePassword(password);
-    if (!responsePassword) throw new Error("Incorrect password");
+    const responsePassword = await user.comparePassword(password)
+    if (!responsePassword) throw new Error('Incorrect password')
 
     // generar jwt
-    const { token, expiresIn } = generateToken(user.id);
-    generateRefreshToken(user.id, res);
+    const { token, expiresIn } = generateToken(user.id)
+    generateRefreshToken(user.id, res)
 
-    user.online = true;
-    await user.save();
+    user.online = true
+    await user.save()
 
-    return res.json({ token, expiresIn });
+    return res.json({ token, expiresIn })
   } catch (error) {
-    return res.status(403).json({ error: error.message });
+    return res.status(403).json({ error: error.message })
   }
-};
+}
 
 const infoUser = async (req, res) => {
   try {
     const user = await User.findById(req.uid, {
-      password: 0,
+      password: 0
       // _id: 0
-    });
-    if (user === null) throw new Error("aqui devuelve null y rompe el front");
-    res.json(user);
+    })
+    if (user === null) throw new Error('aqui devuelve null y rompe el front')
+    res.json(user)
   } catch (error) {
-    return res.status(500).json({ error: "server error" });
+    return res.status(500).json({ error: 'server error' })
   }
-};
+}
 
 const refreshTokenUser = (req, res) => {
   try {
-    const { token, expiresIn } = generateToken(req.uid);
+    const { token, expiresIn } = generateToken(req.uid)
 
-    return res.json({ token, expiresIn });
+    return res.json({ token, expiresIn })
   } catch (error) {
-    return res.status(500).json({ error: "server error" });
+    return res.status(500).json({ error: 'server error' })
   }
-};
+}
 
 const logoutUser = (req, res) => {
-  res.clearCookie("refreshToken");
-  res.json({ ok: true });
-};
+  res.clearCookie('refreshToken')
+  res.json({ ok: true })
+}
 
 const premiumUser = async (req, res) => {
-  const { premium } = req.body;
+  const { premium } = req.body
   const user = await User.findByIdAndUpdate(req.uid, {
-    premium,
-  });
-  await user.save();
-  console.log("El usuario se hizo premium");
-  res.send();
-};
+    premium
+  })
+  await user.save()
+  res.send()
+}
 
 const avatarUser = async (req, res) => {
-  const { avatar } = req.body;
+  const { avatar } = req.body
   const user = await User.findByIdAndUpdate(req.uid, {
-    avatar,
-  });
-  await user.save();
-  console.log(avatar);
-  return res.json({ message: "Avatar cambiado" });
-};
+    avatar
+  })
+  await user.save()
+  console.log(avatar)
+  return res.json({ message: 'Avatar cambiado' })
+}
 
 const setmp3User = async (req, res) => {
-  const { usermp3 } = req.body;
+  const { usermp3 } = req.body
   try {
-    await User.findByIdAndUpdate(req.uid, { $push: { usermp3 } });
-    res.json({ message: "Se guardo el MP3 subido" });
+    await User.findByIdAndUpdate(req.uid, { $push: { usermp3 } })
+    res.json({ message: 'Se guardo el MP3 subido' })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 const favoritesUser = async (req, res) => {
-  const { favorites } = req.body;
+  const { favorites } = req.body
   try {
-    const user = await User.findById(req.uid);
-    const favoriteFound = user.favorites.filter((e) => e.id === favorites.id);
+    const user = await User.findById(req.uid)
+    const favoriteFound = user.favorites.filter((e) => e.id === favorites.id)
     if (favoriteFound.length) {
-      res.json({ message: "Esta cancion ya se encuentra en favoritos" });
+      res.json({ message: 'Esta cancion ya se encuentra en favoritos' })
     } else {
       const userUpdate = await User.findByIdAndUpdate(req.uid, {
-        $push: { favorites },
-      });
-      res.json({ message: "Se guardo la cancion en favoritos" });
+        $push: { favorites }
+      })
+      res.json({ message: 'Se guardo la cancion en favoritos' })
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 //   const user = await User.findById(req.uid, { password: 0})
 //       const filterUser = user.favorites.map(e => e.id)
@@ -185,48 +175,46 @@ const favoritesUser = async (req, res) => {
 // }
 
 const favoritesDelete = async (req, res) => {
-  const { remove } = req.body;
+  const { remove } = req.body
   const user = await User.findByIdAndUpdate(req.uid, {
-    $pull: { favorites: { id: remove } },
-  });
-  await user.save();
-  return res.json({ message: "musica eliminada" });
-};
+    $pull: { favorites: { id: remove } }
+  })
+  await user.save()
+  return res.json({ message: 'musica eliminada' })
+}
 
 const playlistUser = async (req, res) => {
   // const { name } = req.body;
-  const { playlist } = req.body;
-  const userPlaylist = await User.findById(req.uid);
-  const filterUser = userPlaylist.playlists.filter(
-    (e) => e.name === playlist.name
-  );
+  const { playlist } = req.body
+  const userPlaylist = await User.findById(req.uid)
+  const filterUser = userPlaylist.playlists.filter((e) => e.name === playlist.name)
   try {
     if (filterUser.length) {
-      return res.json({ message: "ya existe nombre de playlist" });
+      return res.json({ message: 'ya existe nombre de playlist' })
     } else {
       const playlistUser = await User.findByIdAndUpdate(req.uid, {
-        $push: { playlists: playlist },
-      });
+        $push: { playlists: playlist }
+      })
       // console.log(playlistUser)
-      return res.json({ message: "playlist agregada" });
+      return res.json({ message: 'playlist agregada' })
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
-};
+}
 
 const musicPlaylist = async (req, res) => {
-  const { music, name } = req.body;
-  console.log(music);
+  const { music, name } = req.body
+  console.log(music)
 
   const playlistUser = await User.findByIdAndUpdate(
     req.uid,
-    { $addToSet: { "playlists.$[pls].playlist": music } },
-    { arrayFilters: [{ "pls.name": { $eq: name } }] }
-  );
+    { $addToSet: { 'playlists.$[pls].playlist': music } },
+    { arrayFilters: [{ 'pls.name': { $eq: name } }] }
+  )
 
-  console.log(playlistUser);
-};
+  console.log(playlistUser)
+}
 
 module.exports = {
   validate,
@@ -241,5 +229,5 @@ module.exports = {
   favoritesUser,
   favoritesDelete,
   playlistUser,
-  musicPlaylist,
-};
+  musicPlaylist
+}
--- client/src/components/About/index.jsx
+++ client/src/components/About/index.jsx
@@ -1,95 +1,95 @@
-import NavBarLandingOn from "../LandingPage/NavBarLandingOn";
-import Footer from "../LandingPage/Footer";
-import CardAbout from "./CardAbout";
-import styles from "./about.module.css";
-import stylesLight from "./aboutLight.module.css";
-import NavBarLandingOff from "../LandingPage/NavBarLandingOff";
-import { useSelector } from "react-redux";
+import NavBarLandingOn from '../LandingPage/NavBarLandingOn'
+import Footer from '../LandingPage/Footer'
+import CardAbout from './CardAbout'
+import styles from './about.module.css'
+import stylesLight from './aboutLight.module.css'
+import NavBarLandingOff from '../LandingPage/NavBarLandingOff'
+import { useSelector } from 'react-redux'
 
 function About() {
   let staff = [
     {
-      name: "Jose Casanova",
-      title: "Full Stack Developer",
-      location: "Maracaibo - Venezuela",
+      name: 'Jose Casanova',
+      title: 'Full Stack Developer',
+      location: 'Maracaibo - Venezuela',
       images:
-        "https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661137839/photo_2022-08-22_00-08-07_trhvn9.jpg",
-      linkedin: "https://www.linkedin.com/in/jacasanova98/",
-      github: "https://github.com/Ja-casn"
+        'https://res.cloudinary.com/dyj81r2fi/image/upload/v1663959798/about/photo_2022-08-22_00-08-07_trhvn9_nwelgj.jpg',
+      linkedin: 'https://www.linkedin.com/in/jacasanova98/',
+      github: 'https://github.com/Ja-casn'
     },
     {
-      name: "Hugo Avila",
-      title: "Full Stack Developer",
-      location: "Buenos Aires - Argentina",
+      name: 'Hugo Avila',
+      title: 'Full Stack Developer',
+      location: 'Buenos Aires - Argentina',
       images:
-        "https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661134228/IMG_20220105_110054726_HDR_qejxvm.jpg",
-      linkedin: "https://www.linkedin.com/in/devhugoavila/",
-      github: "https://github.com/hugok2k"
+        'https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661134228/IMG_20220105_110054726_HDR_qejxvm.jpg',
+      linkedin: 'https://www.linkedin.com/in/devhugoavila/',
+      github: 'https://github.com/hugok2k'
     },
     {
-      name: "Santiago Levy",
-      title: "Full Stack Developer",
-      location: "Santiago del Estero - Argentina",
+      name: 'Santiago Levy',
+      title: 'Full Stack Developer',
+      location: 'Santiago del Estero - Argentina',
       images:
-        "https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661134228/photo_2022-08-21_16-04-30_uearmu.jpg",
-      linkedin: "https://www.linkedin.com/in/santiago-levy-dev/",
-      github: "https://github.com/Kosovomba"
+        'https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661134228/photo_2022-08-21_16-04-30_uearmu.jpg',
+      linkedin: 'https://www.linkedin.com/in/santiago-levy-dev/',
+      github: 'https://github.com/Kosovomba'
     },
     {
-      name: "Elam Cano",
-      title: "Full Stack Developer",
-      location: "Corrientes - Argentina",
+      name: 'Elam Cano',
+      title: 'Full Stack Developer',
+      location: 'Corrientes - Argentina',
       images:
-        "https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661133221/photo_2022-08-21_16-00-55_x5irlx.jpg",
-      linkedin: "https://www.linkedin.com/in/elam-cano-bb0419239/",
-      github: "https://github.com/ElamCano"
+        'https://res.cloudinary.com/dyj81r2fi/image/upload/v1663959791/about/photo_2022-08-21_16-00-55_x5irlx_mwfiwx.jpg',
+      linkedin: 'https://www.linkedin.com/in/elam-cano-bb0419239/',
+      github: 'https://github.com/ElamCano'
     },
     {
-      name: "Eduardo Sequeira",
-      title: "Full Stack Developer",
-      location: "Caracas - Venezuela",
-      images: "https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_240/v1661142530/edu_mnvyew.jpg",
-      linkedin: "https://www.linkedin.com/in/eduardo-sequeira/",
-      github: "https://github.com/Eduardoasm"
+      name: 'Eduardo Sequeira',
+      title: 'Full Stack Developer',
+      location: 'Caracas - Venezuela',
+      images: 'https://res.cloudinary.com/dyj81r2fi/image/upload/v1663959783/about/edu_mnvyew_jn3evs.jpg',
+      linkedin: 'https://www.linkedin.com/in/eduardo-sequeira/',
+      github: 'https://github.com/Eduardoasm'
     },
     {
-      name: "Gabriel Sanchez",
-      title: "Full Stack Developer",
-      location: "Buenos Aires - Argentina",
-      images: "https://res.cloudinary.com/hugok2k/image/upload/c_scale,h_518/v1661142963/gaby_fgdtk9.jpg",
-      linkedin: "https://www.linkedin.com/in/gabriel-sanchez-0591a723a/",
-      github: "https://github.com/gszcode"
+      name: 'Gabriel Sanchez',
+      title: 'Full Stack Developer',
+      location: 'Buenos Aires - Argentina',
+      images: 'https://res.cloudinary.com/dyj81r2fi/image/upload/v1663959775/about/gaby_fgdtk9_qwvu6h.jpg',
+      linkedin: 'https://www.linkedin.com/in/gabriel-sanchez-0591a723a/',
+      github: 'https://github.com/gszcode'
     },
     {
-      name: "Alejandro Maturrano",
-      title: "Full Stack Developer",
-      location: "Lima, Peru",
-      images: "https://res.cloudinary.com/hugok2k/image/upload/v1661916969/alejandro_leodxr.jpg",
-      linkedin: "https://www.linkedin.com/in/tm10ymhp/",
-      github: "https://github.com/TM10YMhp"
+      name: 'Alejandro Maturrano',
+      title: 'Full Stack Developer',
+      location: 'Lima, Peru',
+      images: 'https://res.cloudinary.com/dyj81r2fi/image/upload/v1663959701/about/alejandro_n2rvls.jpg',
+      linkedin: 'https://www.linkedin.com/in/tm10ymhp/',
+      github: 'https://github.com/TM10YMhp'
     }
-  ];
+  ]
 
   const shuffle = function (deck) {
-    let deckShuffled = [];
+    let deckShuffled = []
     while (deck.length > 0) {
-      let cardIndex = Math.floor(Math.random() * deck.length);
-      let card = deck[cardIndex];
-      deckShuffled.push(card);
-      deck.splice(cardIndex, 1);
+      let cardIndex = Math.floor(Math.random() * deck.length)
+      let card = deck[cardIndex]
+      deckShuffled.push(card)
+      deck.splice(cardIndex, 1)
     }
-    return deckShuffled;
-  };
-  staff = shuffle(staff);
+    return deckShuffled
+  }
+  staff = shuffle(staff)
 
-  const { user } = useSelector((state) => state.user);
-  const theme = localStorage.getItem("theme");
+  const { user } = useSelector((state) => state.user)
+  const theme = localStorage.getItem('theme')
 
   return (
     <>
       {Object.keys(user).length ? <NavBarLandingOn /> : <NavBarLandingOff />}
 
-      <div className={theme === "light" ? stylesLight.containerAbout : styles.containerAbout}>
+      <div className={theme === 'light' ? stylesLight.containerAbout : styles.containerAbout}>
         {staff.map((e) => (
           <div className="container-about" key={e.name}>
             <CardAbout
@@ -105,7 +105,7 @@ function About() {
       </div>
       <Footer />
     </>
-  );
+  )
 }
 
-export default About;
+export default About
